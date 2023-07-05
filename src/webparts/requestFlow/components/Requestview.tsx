23edf3cfc6901a98a2e195b3e86dc0ffcac09c2b
Flynt Gao <flynt.gao@consultant.udtrucks.com>
2023-07-05 15:11:31 +0800

    change homepage link

--- src/webparts/requestFlow/components/Requestview.tsx
+++ src/webparts/requestFlow/components/Requestview.tsx
@@ -259,7 +259,7 @@ export default function RequestView(): JSX.Element {
     addRequest({ request }).then(() => {
       const returnUrl = window.location.href
 
-      document.location.href = returnUrl.slice(0, returnUrl.indexOf("SitePage")) + "Request.aspx"
+      document.location.href = returnUrl.slice(0, returnUrl.indexOf("SitePage")) + "SitePages/Home.aspx"
     }).catch((error) => console.log(error))
 
   }
@@ -374,7 +374,7 @@ export default function RequestView(): JSX.Element {
         <DefaultButton onClick={() => {
           const returnUrl = window.location.href
 
-          document.location.href = returnUrl.slice(0, returnUrl.indexOf("SitePage")) + "Request.aspx"
+          document.location.href = returnUrl.slice(0, returnUrl.indexOf("SitePage")) + "SitePage/Request.aspx"
         }} text="Cancel" />
       </Stack>
       {dialogvisible ?
