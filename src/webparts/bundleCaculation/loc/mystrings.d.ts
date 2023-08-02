8660fb1c083d61d7bfcd9f6559b79c22c75e8fdd
Flynt Gao <flynt.gao@consultant.udtrucks.com>
2023-08-02 13:31:41 +0800

    Add caculate View

---
+++ src/webparts/bundleCaculation/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IBundleCaculationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'BundleCaculationWebPartStrings' {
+  const strings: IBundleCaculationWebPartStrings;
+  export = strings;
+}
